79c32a7eced5595f7dca5de4cb3ea8d78d9789e3
Gabriel Nóbrega <matiasnobrega7@gmail.com>
2019-03-18 15:09:15 -0300

    FormulárioAdmUsuario

--- frontend/src/components/template/Header.vue
+++ frontend/src/components/template/Header.vue
@@ -38,8 +38,8 @@ export default {
     .header{
         grid-area: header;
         /*background: linear-gradient(to right, #1e469a, #49a7c1);*/
-        /*background-image: radial-gradient( circle farthest-corner at 12.3% 19.3%,  rgba(85,88,218,1) 0%, rgba(95,209,249,1) 100.2% );*/
-        background-image: radial-gradient( circle farthest-corner at -4% -12.9%,  rgba(74,98,110,1) 0.3%, rgba(30,33,48,1) 90.2% );
+        background-image: radial-gradient( circle farthest-corner at 12.3% 19.3%,  rgba(85,88,218,1) 0%, rgba(95,209,249,1) 100.2% );
+        /*background-image: radial-gradient( circle farthest-corner at -4% -12.9%,  rgba(74,98,110,1) 0.3%, rgba(30,33,48,1) 90.2% );
         /*background-image: radial-gradient( circle farthest-corner at 85.4% 50.8%,  rgba(14,72,222,1) 0%, rgba(3,22,65,1) 74.2% );*/
         display: flex;
         justify-content: center;
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,7 +11,7 @@ import router from './config/router'
 Vue.config.productionTip = false
 
 // Token no header da requisição temporariamente
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkdhYnJpZWwgTWF0aWFzIGRhIE7Ds2JyZWdhIiwiZW1haWwiOiJtYXRpYXNub2JyZWdhN0BnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTUyMzY4Mzc5LCJleHAiOjE1NTI2Mjc1Nzl9.6SCPw_9-K2D6wtX4irs1dDEgRrkCBg7cajXpVjbviFI'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkdhYnJpZWwgTWF0aWFzIGRhIE7Ds2JyZWdhIiwiZW1haWwiOiJtYXRpYXNub2JyZWdhN0BnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTUyOTMxOTg2LCJleHAiOjE1NTMxOTExODZ9.QvAUGWIxNRq-RxcFslObjo_-A4U7Tb0fymKFGSkHpn4'
 
 new Vue({
   store,
