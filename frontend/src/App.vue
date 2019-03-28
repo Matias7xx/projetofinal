295a84214f15f67acb5b3f9606e4b0a7b96cbd26
Gabriel Nóbrega <matiasnobrega7@gmail.com>
2019-03-28 14:51:08 -0300

    Validar Token

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,22 +4,59 @@
 			:hideToggle="!user"
 			:hideUserDropdown="!user" />
 		<Menu v-if="user" />
-		<Content />
+		<Loading v-if="validateToken" />
+		<Content v-else />
 		<Footer />
 	</div>
 </template>
 
 <script>
+import axios from 'axios'
+import { baseApiUrl, userKey } from '@/global'
 import { mapState } from 'vuex'
 import Header from "@/components/template/Header"
 import Menu from "@/components/template/Menu"
 import Content from "@/components/template/Content"
 import Footer from "@/components/template/Footer"
+import Loading from "@/components/template/Loading"
 
 export default {
 	name: "App",
-	components: { Header, Menu, Content, Footer },
-	computed: mapState(['isMenuVisible', 'user']) //Mapear visibilidade do Menu e Usuário
+	components: { Header, Menu, Content, Footer, Loading },
+	computed: mapState(['isMenuVisible', 'user']), //Mapear visibilidade do Menu e Usuário
+	data: function() {
+		return {
+			validatingToken: true //Validação de Token
+		}
+	},
+	methods: {
+		async validateToken() {
+			this.validatingToken = true
+
+			const json = localStorage.getItem(userKey)
+			const userData = JSON.parse(json)
+			this.$store.commit('setUser', null)
+
+			if(!userData) { //Se o usuário não estiver setado
+				this.validatingToken = false
+				return this.$router.push({ name: 'auth' })
+			}
+
+			const res = await axios.post(`${baseApiUrl}/validateToken`, userData)
+
+			if (res.data) {
+				this.$store.commit('setUser', userData)
+			} else {
+				localStorage.removeItem(userKey)
+				this.$router.push({ name: 'auth' })
+			}
+
+			this.validatingToken = false
+		}
+	},
+	created() {
+		this.validateToken()
+	}
 }
 </script>
 
